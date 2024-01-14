6db238e01894354faf1b06cea21fe874dd56053a
muhsin <egemuhsin@gmail.com>
2024-01-15 00:15:27 +0300

    refactor: json load on device, lanscape auto rotation

--- Assets/Scripts/Utils/LevelSaveSystem.cs
+++ Assets/Scripts/Utils/LevelSaveSystem.cs
@@ -9,15 +9,19 @@ namespace Utils
     {
         public static List<GridData> LoadLevels()
         {
-            var json =
-                System.IO.File.ReadAllText(Application.dataPath + $"/LevelDesign/LevelData.json");
+#if UNITY_EDITOR
+            var json = System.IO.File.ReadAllText(Application.dataPath + $"/Resources/LevelData.json");
+#else
+            var json = Resources.Load<TextAsset>("LevelData").text;
+#endif
+
             return JsonConvert.DeserializeObject<List<GridData>>(json);
         }
-        
+
         public static void SaveLevels(List<GridData> levelGrid)
         {
             string json = JsonConvert.SerializeObject(levelGrid);
-            System.IO.File.WriteAllText(Application.dataPath + $"/LevelDesign/LevelData.json", json);
+            System.IO.File.WriteAllText(Application.dataPath + $"/Resources/LevelData.json", json);
         }
     }
 }
